2d37b22a33e48ed862102c56619f35502580341d
hyorish03 <hyorish2001@gmail.com>
2024-09-14 01:34:06 +0900

    Chore: eslint import 순서 강제 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,18 @@ module.exports = {
         extensions: ['.jsx', '.tsx', '.js'],
       },
     ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['builtin', 'external'], // 내장 모듈과 외부 모듈을 먼저
+          ['internal'], // 내부 모듈
+          ['parent', 'sibling', 'index'], // 상위/형제/인덱스 모듈 순서로
+        ],
+        'newlines-between': 'always', // 그룹 간 줄바꿈을 강제
+        alphabetize: { order: 'asc', caseInsensitive: true }, // 알파벳 순으로 정렬
+      },
+    ],
     'react-native/no-unused-styles': 2,
     'react-native/split-platform-components': 2,
     'react-native/no-inline-styles': 2,
