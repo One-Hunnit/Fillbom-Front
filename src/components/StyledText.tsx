fda37bd8981efdded1b8e5735e25320b4b936189
sjsjsj1246 <sjsjsj1246@gmail.com>
2024-10-01 16:43:58 +0900

    feat(OH-105): firebase 연동, 앱 다시 빌드

--- src/components/StyledText.tsx
+++ src/components/StyledText.tsx
@@ -3,5 +3,5 @@ import { Text } from './Themed';
 
 export function MonoText(props: TextProps) {
   // eslint-disable-next-line react-native/no-inline-styles
-  return <Text {...props} style={[props.style, { fontFamily: 'SpaceMono' }]} />;
+  return <Text {...props} style={[props.style]} />;
 }
