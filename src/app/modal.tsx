5615d161da7b72d94e09995aa9f8472eb25384d1
황인서[SE][SmartEditor] <inseo.hwang@navercorp.com>
2024-10-05 16:55:17 +0900

    feat(-): 안쓰이는 web용 파일, 다크모드 제거, 프린트 코드 길이 수정

--- src/app/modal.tsx
+++ src/app/modal.tsx
@@ -1,17 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
 
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Modal</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
+      <View style={styles.separator} />
       <EditScreenInfo path="app/modal.tsx" />
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
@@ -27,6 +22,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   separator: {
+    backgroundColor: 'rgba(255,255,255,0.1)',
     height: 1,
     marginVertical: 30,
     width: '80%',
--- src/components/StyledText.tsx
+++
@@ -1,7 +0,0 @@
-import type { TextProps } from './Themed';
-import { Text } from './Themed';
-
-export function MonoText(props: TextProps) {
-  // eslint-disable-next-line react-native/no-inline-styles
-  return <Text {...props} style={[props.style]} />;
-}
