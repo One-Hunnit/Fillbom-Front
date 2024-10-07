bd2e21a4ea1831c35ce599ac3863494f99ac949a
hyorish03 <hyorish2001@gmail.com>
2024-10-07 20:17:05 +0900

    feat(OH-94): 위치 추적 함수 및 TaskManager 적용

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,28 @@
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { backgroundLocationTraking } from './(locationUtils)/backgroundLocationTraking';
+import { defineLocationTrackingTask } from './(locationUtils)/defineLocationTrackingTask';
+import { foreGroundLocationTraking } from './(locationUtils)/foreGroundLocationTraking';
+import { requestLocationPermissions } from './(locationUtils)/requestLocationPermissions';
 
 export default function Home() {
+  useEffect(() => {
+    (async () => {
+      const hasPermission = await requestLocationPermissions();
+      if (!hasPermission) return;
+      defineLocationTrackingTask();
+      await foreGroundLocationTraking();
+      await backgroundLocationTraking();
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Home Page</Text>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
