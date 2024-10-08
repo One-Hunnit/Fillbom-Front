769f7261adaf8631990a8346a05e3f41cb831b81
hyorish03 <hyorish2001@gmail.com>
2024-10-09 02:11:29 +0900

    feat(OH-108): pretendard 폰트 적용

---
+++ src/styles/textStyles.ts
@@ -0,0 +1,132 @@
+/* eslint-disable react-native/sort-styles */
+import { StyleSheet } from 'react-native';
+
+const textStyles = StyleSheet.create({
+  // Title Styles
+  TITLE_XL_BOLD: {
+    fontFamily: 'Pretendard-Bold',
+    fontSize: 26,
+    fontWeight: 'bold',
+    lineHeight: 36,
+  },
+  TITLE_XL_SEMI_BOLD: {
+    fontFamily: 'Pretendard-SemiBold',
+    fontSize: 26,
+    fontWeight: '600',
+    lineHeight: 36, // SemiBold
+  },
+  TITLE_XL_REGULAR: {
+    fontFamily: 'Pretendard-Regular',
+    fontSize: 26,
+    fontWeight: 'normal',
+    lineHeight: 36,
+  },
+
+  // SubTitle Styles
+  SUBTITLE_LARGE_BOLD: {
+    fontFamily: 'Pretendard-Bold',
+    fontSize: 20,
+    fontWeight: 'bold',
+    lineHeight: 28,
+  },
+  SUBTITLE_LARGE_SEMI_BOLD: {
+    fontFamily: 'Pretendard-SemiBold',
+    fontSize: 20,
+    fontWeight: '600',
+    lineHeight: 28, // SemiBold
+  },
+  SUBTITLE_LARGE_REGULAR: {
+    fontFamily: 'Pretendard-Regular',
+    fontSize: 20,
+    fontWeight: 'normal',
+    lineHeight: 28,
+  },
+
+  // Body Styles
+  BODY_MEDIUM_BOLD: {
+    fontFamily: 'Pretendard-Bold',
+    fontSize: 16,
+    fontWeight: 'bold',
+    lineHeight: 22,
+  },
+  BODY_MEDIUM_SEMI_BOLD: {
+    fontFamily: 'Pretendard-SemiBold',
+    fontSize: 16,
+    fontWeight: '600',
+    lineHeight: 22, // SemiBold
+  },
+  BODY_MEDIUM_MEDIUM: {
+    fontFamily: 'Pretendard-Medium',
+    fontSize: 16,
+    fontWeight: '500',
+    lineHeight: 22, // Medium
+  },
+  BODY_MEDIUM_REGULAR: {
+    fontFamily: 'Pretendard-Regular',
+    fontSize: 16,
+    fontWeight: 'normal',
+    lineHeight: 22,
+  },
+
+  // SubText Styles
+  SUBTEXT_SMALL_BOLD: {
+    fontFamily: 'Pretendard-Bold',
+    fontSize: 14,
+    fontWeight: 'bold',
+    lineHeight: 20,
+  },
+  SUBTEXT_SMALL_SEMI_BOLD: {
+    fontFamily: 'Pretendard-SemiBold',
+    fontSize: 14,
+    fontWeight: '600',
+    lineHeight: 20, // SemiBold
+  },
+  SUBTEXT_SMALL_MEDIUM: {
+    fontFamily: 'Pretendard-Medium',
+    fontSize: 14,
+    fontWeight: '500',
+    lineHeight: 20, // Medium
+  },
+  SUBTEXT_SMALL_REGULAR: {
+    fontFamily: 'Pretendard-Regular',
+    fontSize: 14,
+    fontWeight: 'normal',
+    lineHeight: 20,
+  },
+
+  // Sentence Styles
+  SENTENCE_SMALL_REGULAR: {
+    fontFamily: 'Pretendard-Regular',
+    fontSize: 14,
+    fontWeight: 'normal',
+    lineHeight: 21, // 150% of 14
+  },
+
+  // Caption Styles
+  CAPTION_X_SMALL_BOLD: {
+    fontFamily: 'Pretendard-Bold',
+    fontSize: 12,
+    fontWeight: 'bold',
+    lineHeight: 18,
+  },
+  CAPTION_X_SMALL_SEMI_BOLD: {
+    fontFamily: 'Pretendard-SemiBold',
+    fontSize: 12,
+    fontWeight: '600',
+    lineHeight: 18, // SemiBold
+  },
+  CAPTION_X_SMALL_MEDIUM: {
+    fontFamily: 'Pretendard-Medium',
+    fontSize: 12,
+    fontWeight: '500',
+    lineHeight: 18, // Medium
+  },
+  CAPTION_X_SMALL_REGULAR: {
+    fontFamily: 'Pretendard-Regular',
+    fontSize: 12,
+    fontWeight: 'normal',
+    lineHeight: 18,
+  },
+});
+
+export default textStyles;
