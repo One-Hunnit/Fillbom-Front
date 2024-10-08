ec5d1fc12a2903600432178be99d12717f60f8ff
hyorish03 <hyorish2001@gmail.com>
2024-10-08 23:58:16 +0900

    feat(OH-131): 네이버지도 현 위치 마커 구현

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,24 +1,67 @@
-import { NaverMapView } from '@mj-studio/react-native-naver-map';
-import { useEffect } from 'react';
+import { NaverMapMarkerOverlay, NaverMapView } from '@mj-studio/react-native-naver-map';
+import { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import currentLocationIcon from '@/assets/images/png/currentLocationIcon.png';
 import { backgroundLocationTraking } from './(locationUtils)/backgroundLocationTraking';
 import { defineLocationTrackingTask } from './(locationUtils)/defineLocationTrackingTask';
 import { foreGroundLocationTraking } from './(locationUtils)/foreGroundLocationTraking';
 import { requestLocationPermissions } from './(locationUtils)/requestLocationPermissions';
+interface LocationCoords {
+  latitude: number;
+  longitude: number;
+}
 
+interface Location {
+  latitude: number;
+  longitude: number;
+}
+
+interface Coordinates {
+  accuracy: number | null;
+  altitude: number | null;
+  altitudeAccuracy: number | null;
+  heading: number | null;
+  latitude: number;
+  longitude: number;
+  speed: number | null;
+}
 export default function Home() {
+  const [locationCoords, setLocationCoords] = useState<LocationCoords>({
+    latitude: 37.6540767,
+    longitude: 127.0566033,
+  });
   useEffect(() => {
     (async () => {
       const hasPermission = await requestLocationPermissions();
       if (!hasPermission) return;
       defineLocationTrackingTask();
-      await foreGroundLocationTraking();
+      const location: Coordinates = await foreGroundLocationTraking();
+      setLocationCoords({ latitude: location.latitude, longitude: location.longitude });
       await backgroundLocationTraking();
     })();
   }, []);
   return (
     <View style={styles.container}>
-      <NaverMapView style={styles.mapContainer} />
+      <NaverMapView
+        initialRegion={{
+          latitude: locationCoords.latitude,
+          longitude: locationCoords.longitude,
+          latitudeDelta: 0.0022,
+          longitudeDelta: 0.0022,
+        }}
+        isShowScaleBar={false}
+        isShowZoomControls={false}
+        style={styles.mapContainer}
+      >
+        <NaverMapMarkerOverlay
+          latitude={locationCoords.latitude}
+          longitude={locationCoords.longitude}
+          anchor={{ x: 0.5, y: 1 }}
+          width={40}
+          height={40}
+          image={currentLocationIcon}
+        ></NaverMapMarkerOverlay>
+      </NaverMapView>
     </View>
   );
 }
