9c93fb2452b31807875735559ebaa7cf21c20cd4
sjsjsj1246 <sjsjsj1246@gmail.com>
2024-10-09 11:26:14 +0900

    fix(-): 경로 수정

--- src/hooks/useInitializeApp.ts
+++ src/hooks/useInitializeApp.ts
@@ -2,15 +2,15 @@ import { useFonts } from 'expo-font';
 
 export default function useInitializeApp() {
   const [assetsLoaded, loadAssetError] = useFonts({
-    PretendardBlack: require('./assets/fonts/Pretendard-Black.otf'),
-    PretendardBold: require('./assets/fonts/Pretendard-Bold.otf'),
-    PretendardExtraBold: require('./assets/fonts/Pretendard-ExtraBold.otf'),
-    PretendardExtraLight: require('./assets/fonts/Pretendard-ExtraLight.otf'),
-    PretendardLight: require('./assets/fonts/Pretendard-Light.otf'),
-    PretendardMedium: require('./assets/fonts/Pretendard-Medium.otf'),
-    PretendardRegular: require('./assets/fonts/Pretendard-Regular.otf'),
-    PretendardSemiBold: require('./assets/fonts/Pretendard-SemiBold.otf'),
-    PretendardThin: require('./assets/fonts/Pretendard-Thin.otf'),
+    PretendardBlack: require('../assets/fonts/Pretendard-Black.otf'),
+    PretendardBold: require('../assets/fonts/Pretendard-Bold.otf'),
+    PretendardExtraBold: require('../assets/fonts/Pretendard-ExtraBold.otf'),
+    PretendardExtraLight: require('../assets/fonts/Pretendard-ExtraLight.otf'),
+    PretendardLight: require('../assets/fonts/Pretendard-Light.otf'),
+    PretendardMedium: require('../assets/fonts/Pretendard-Medium.otf'),
+    PretendardRegular: require('../assets/fonts/Pretendard-Regular.otf'),
+    PretendardSemiBold: require('../assets/fonts/Pretendard-SemiBold.otf'),
+    PretendardThin: require('../assets/fonts/Pretendard-Thin.otf'),
   });
 
   // @TODO 추후 GA, Sentry 등 초기화 로직 추가
