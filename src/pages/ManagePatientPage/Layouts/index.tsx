242ae6bb2a90abef6e80e82b300cd24a219e39ec
hyorish03 <hyorish2001@gmail.com>
2025-01-21 20:02:12 +0900

    feat(OH-116): ManagePatientLayout 컴포넌트에 키보드 높이 인식 기능 추가 및 스타일 개선

--- src/pages/ManagePatientPage/Layouts/index.tsx
+++ src/pages/ManagePatientPage/Layouts/index.tsx
@@ -1,38 +1,54 @@
+import { WINDOW_HEIGHT } from '@gorhom/bottom-sheet';
 import React from 'react';
 import { Keyboard, Pressable, Text, View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '@/components/Header';
-import { commonStyles } from '../styles';
+import { HEADER_HEIGHT } from '@/constants/ui';
+import { commonStyles } from './styles';
 
 interface ManagePatientLayoutProps {
   headerText: string;
   setIsInputFocused: (value: boolean) => void;
-  children: React.ReactNode; // ReactNode로 변경하여 배열과 단일 컴포넌트 모두 허용
+  children: React.ReactNode;
   titleText: string;
+  status: 'REQUEST' | 'ADD';
 }
 
-const ManagePatientLayout = ({ headerText, setIsInputFocused, children, titleText }: ManagePatientLayoutProps) => {
+const ManagePatientLayout = ({
+  headerText,
+  setIsInputFocused,
+  children,
+  titleText,
+  status,
+}: ManagePatientLayoutProps) => {
   const childrenArray = React.Children.toArray(children);
-
+  const PRESSABLE_HEIGHT = status === 'REQUEST' ? WINDOW_HEIGHT - HEADER_HEIGHT - 88 : 116 + 142;
   return (
-    <SafeAreaView style={commonStyles.safeArea} edges={['left', 'right', 'top']}>
-      <Header backButtonVisible={true} containerStyle={commonStyles.headerContainer} title={headerText} />
-      <View style={commonStyles.pageWrapper}>
-        <Pressable
-          onPress={() => {
-            setIsInputFocused(false);
-            Keyboard.dismiss();
-          }}
-          style={commonStyles.pressableContainer}
-        >
-          <View style={commonStyles.titleWrapper}>
-            <Text style={commonStyles.title}>{titleText}</Text>
+    <KeyboardAwareScrollView contentContainerStyle={commonStyles.safeArea}>
+      <SafeAreaView style={commonStyles.safeArea} edges={['left', 'right', 'top', 'bottom']}>
+        <Header backButtonVisible={true} containerStyle={commonStyles.headerContainer} title={headerText} />
+        <View style={commonStyles.pageWrapper}>
+          <Pressable
+            onPress={() => {
+              setIsInputFocused(false);
+              Keyboard.dismiss();
+            }}
+            style={[commonStyles.pressableContainer, { height: PRESSABLE_HEIGHT }]}
+          >
+            <View>
+              <View style={commonStyles.titleWrapper}>
+                <Text style={commonStyles.title}>{titleText}</Text>
+              </View>
+              {childrenArray[0]}
+            </View>
+          </Pressable>
+          <View style={[commonStyles.example, { height: WINDOW_HEIGHT - HEADER_HEIGHT - PRESSABLE_HEIGHT }]}>
+            {childrenArray[1]}
           </View>
-          {childrenArray[0]}
-        </Pressable>
-        {childrenArray[1]}
-      </View>
-    </SafeAreaView>
+        </View>
+      </SafeAreaView>
+    </KeyboardAwareScrollView>
   );
 };
 
