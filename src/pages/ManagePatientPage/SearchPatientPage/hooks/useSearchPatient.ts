44610e1546465b13f1ad169b68271b7138371951
hyorish03 <hyorish2001@gmail.com>
2025-02-11 14:18:06 +0900

    feat(OH-116): 불필요한 콘솔로그 삭제

--- src/pages/ManagePatientPage/SearchPatientPage/hooks/useSearchPatient.ts
+++ src/pages/ManagePatientPage/SearchPatientPage/hooks/useSearchPatient.ts
@@ -6,9 +6,7 @@ export default function useSearchPatient() {
   const [patientList, setPatientList] = useState<IPatientInfo[] | null>(null);
 
   const postSearchPatient = async (phoneNumber: string) => {
-    console.log('phine');
     const { data } = await client.POST('/patients/search', { body: { phoneNumber } });
-    console.log(data);
     if (data?.data && data.data.length > 0) {
       setPatientList(data.data as IPatientInfo[]);
     } else {
