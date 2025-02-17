7510ea47a65e470dd3032e231a4a3875c5cf048e
hyorish03 <hyorish2001@gmail.com>
2025-02-17 18:54:52 +0900

    feat(OH-116): 환자 등록 기능을 useMutation으로 변경 및 오류 처리 추가

--- src/pages/ManagePatientPage/RequestRelationPage/hooks/useRegistPatients.ts
+++ src/pages/ManagePatientPage/RequestRelationPage/hooks/useRegistPatients.ts
@@ -1,16 +1,22 @@
+import { useMutation } from '@tanstack/react-query';
 import { client } from '@/api/client';
 
-const useRegistPatients = async (patientId: number, relationship: string) => {
-  const { data } = await client.POST(`/caregiver/registration/patients/{patientId}`, {
-    params: {
-      path: {
-        patientId,
-      },
-    },
-    body: {
-      relationship: relationship,
-    },
+const registerPatient = async (patientId: number, relationship: string) => {
+  const { data, error } = await client.POST('/caregiver/registration/patients/{patientId}', {
+    params: { path: { patientId } },
+    body: { relationship },
   });
-  return { data: data?.data, status: data?.status };
+
+  if (error) {
+    throw new Error(error?.message || '환자 등록 실패');
+  }
+
+  return data;
 };
-export default useRegistPatients;
+
+export const useRegisterPatient = () => {
+  return useMutation({
+    mutationFn: ({ patientId, relationship }: { patientId: number; relationship: string }) =>
+      registerPatient(patientId, relationship),
+  });
+};
