0bebbbc2023975e0d6fa26a4a4aafdf55e10ee8a
hyorish03 <hyorish2001@gmail.com>
2025-02-17 19:16:43 +0900

    feat(OH-116): ManagePatientPage에서 키보드 해제를 위한 Pressable 컴포넌트 추가

--- src/pages/ManagePatientPage/Layouts/index.tsx
+++ src/pages/ManagePatientPage/Layouts/index.tsx
@@ -1,6 +1,6 @@
 import { WINDOW_HEIGHT } from '@gorhom/bottom-sheet';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Keyboard, Pressable, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '@/components/Header';
 import { HEADER_HEIGHT } from '@/constants/ui';
@@ -22,14 +22,17 @@ const ManagePatientLayout = ({ headerText, children, titleText, status }: Manage
       <SafeAreaView style={commonStyles.safeArea} edges={['left', 'right', 'top', 'bottom']}>
         <Header backButtonVisible={true} containerStyle={commonStyles.headerContainer} title={headerText} />
         <View style={commonStyles.pageWrapper}>
-          <View style={[commonStyles.pressableContainer, { height: PRESSABLE_HEIGHT }]}>
+          <Pressable
+            onPress={() => Keyboard.dismiss()}
+            style={[commonStyles.pressableContainer, { height: PRESSABLE_HEIGHT }]}
+          >
             <View>
               <View style={commonStyles.titleWrapper}>
                 <Text style={commonStyles.title}>{titleText}</Text>
               </View>
               {childrenArray[0]}
             </View>
-          </View>
+          </Pressable>
           <View style={[commonStyles.example, { height: WINDOW_HEIGHT - HEADER_HEIGHT - PRESSABLE_HEIGHT }]}>
             {childrenArray[1]}
           </View>
