9ccb046a260fc59bfc4e137247159c9c5e53c34f
hyorish03 <hyorish2001@gmail.com>
2025-02-18 13:50:20 +0900

    feat(OK-23): 환자 상세보기 페이지에 수정 및 삭제 메뉴 추가 및 스타일링

--- src/pages/PatientDetailPage/index.tsx
+++ src/pages/PatientDetailPage/index.tsx
@@ -1,10 +1,14 @@
+/* eslint-disable react-native/no-inline-styles */
 import { useState } from 'react';
-import { Dimensions, Pressable, Text, View } from 'react-native';
+import { Dimensions, Pressable, Text } from 'react-native';
 import { Divider, Menu, PaperProvider } from 'react-native-paper';
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { indexStyle } from './styles/index.style';
+import CloseNormal from '@/assets/svgs/ico_close_normal.svg';
+import CorrectNormal from '@/assets/svgs/ico_correct_normal.svg';
 import Header from '@/components/Header';
 import CMenu from '@/components/Menu';
+import { FILLBOM_COLOR } from '@/constants/color';
 const PatientDetailPage = () => {
   const [isRightIconVisible, setIsRightIconVisible] = useState(false);
   const openMenu = () => {
@@ -32,18 +36,28 @@ const PatientDetailPage = () => {
           containerStyle={indexStyle.headerContainer}
           title="환자 상세보기"
         />
-        <View>
-          <Menu
-            visible={isRightIconVisible}
-            onDismiss={closeMenu}
-            anchorPosition="bottom"
-            anchor={{ x: width - 20, y: insets.top + 40 }}
-          >
-            <Menu.Item onPress={() => {}} title="수정하기" />
-            <Divider />
-            <Menu.Item onPress={() => {}} title="삭제하기" />
-          </Menu>
-        </View>
+
+        <Menu
+          contentStyle={indexStyle.menu}
+          visible={isRightIconVisible}
+          onDismiss={closeMenu}
+          anchorPosition="bottom"
+          anchor={{ x: width - 20, y: insets.top + 40 }}
+        >
+          <Menu.Item
+            style={[indexStyle.menuItem, { borderTopLeftRadius: 12, borderTopRightRadius: 12 }]}
+            trailingIcon={CorrectNormal}
+            onPress={() => {}}
+            title="수정하기"
+          />
+          <Divider style={{ borderColor: FILLBOM_COLOR.GRAY[200] }} />
+          <Menu.Item
+            style={[indexStyle.menuItem, { borderBottomLeftRadius: 12, borderBottomRightRadius: 12 }]}
+            onPress={() => {}}
+            trailingIcon={CloseNormal}
+            title="삭제하기"
+          />
+        </Menu>
         <Text>PatientDetailPage</Text>
         <Pressable onPress={handleRightIconPress}>
           <Text>Back</Text>
