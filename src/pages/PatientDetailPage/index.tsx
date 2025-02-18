60d790f0ff7341d376713d3b591d9cc7058cfe7b
hyorish03 <hyorish2001@gmail.com>
2025-02-18 12:59:30 +0900

    feat(OK-23): 환자 상세보기 페이지에 메뉴 및 스타일 추가

--- src/pages/PatientDetailPage/index.tsx
+++ src/pages/PatientDetailPage/index.tsx
@@ -1,14 +1,55 @@
-import { router } from 'expo-router';
-import { Pressable, Text, View } from 'react-native';
-
+import { useState } from 'react';
+import { Dimensions, Pressable, Text, View } from 'react-native';
+import { Divider, Menu, PaperProvider } from 'react-native-paper';
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { indexStyle } from './styles/index.style';
+import Header from '@/components/Header';
+import CMenu from '@/components/Menu';
 const PatientDetailPage = () => {
+  const [isRightIconVisible, setIsRightIconVisible] = useState(false);
+  const openMenu = () => {
+    console.log('openMenu');
+    setIsRightIconVisible(true);
+  };
+  const closeMenu = () => {
+    setIsRightIconVisible(false);
+  };
+
+  const insets = useSafeAreaInsets();
+  const { width } = Dimensions.get('window');
+
+  const handleRightIconPress = () => {
+    //수정 삭제 모달 오픈
+  };
   return (
-    <View>
-      <Text>PatientDetailPage</Text>
-      <Pressable onPress={() => router.back()}>
-        <Text>Back</Text>
-      </Pressable>
-    </View>
+    <PaperProvider>
+      <SafeAreaView style={indexStyle.safeArea} edges={['left', 'right', 'top', 'bottom']}>
+        <Header
+          backButtonVisible={true}
+          rightIcon={<CMenu onPress={openMenu} />}
+          onRightIconPress={openMenu}
+          isRightIconVisible={true}
+          containerStyle={indexStyle.headerContainer}
+          title="환자 상세보기"
+        />
+        <View>
+          <Menu
+            visible={isRightIconVisible}
+            onDismiss={closeMenu}
+            anchorPosition="bottom"
+            anchor={{ x: width - 20, y: insets.top + 40 }}
+          >
+            <Menu.Item onPress={() => {}} title="수정하기" />
+            <Divider />
+            <Menu.Item onPress={() => {}} title="삭제하기" />
+          </Menu>
+        </View>
+        <Text>PatientDetailPage</Text>
+        <Pressable onPress={handleRightIconPress}>
+          <Text>Back</Text>
+        </Pressable>
+      </SafeAreaView>
+    </PaperProvider>
   );
 };
 export default PatientDetailPage;
---
+++ src/pages/PatientDetailPage/styles/index.style.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const indexStyle = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#fff',
+  },
+  headerContainer: {
+    borderBottomWidth: 0,
+    borderBottomColor: 'none',
+  },
+});
