fa580cf04c1224e09b1d900b0280339df4a7a519
hyorish03 <hyorish2001@gmail.com>
2025-02-18 18:40:16 +0900

    feat(OK-23): 환자 상세보기 페이지에 환자 정보 카드 컴포넌트 추가

---
+++ src/pages/PatientDetailPage/components/PatientInfoCard.tsx
@@ -0,0 +1,43 @@
+import { Image, Text, View } from 'react-native';
+import { type PatientDetailData } from '../hooks/useGetPatientDetail';
+import patientInfoCardStyle from '../styles/patientInfoCard.style';
+
+interface PatientInfoCardProps {
+  patientInfo: PatientDetailData;
+}
+const PatientInfoCard = ({ patientInfo }: PatientInfoCardProps) => {
+  const gender = patientInfo?.gender === 'MAN' ? '남' : '여';
+  return (
+    <View style={patientInfoCardStyle.Wrapper}>
+      <Image src={patientInfo?.profileImageUrl} style={patientInfoCardStyle.profileImage} />
+      <View style={patientInfoCardStyle.patientInfoTextWrapper}>
+        {patientInfo?.name && (
+          <View style={patientInfoCardStyle.labelValueWrapper}>
+            <Text style={patientInfoCardStyle.label}>이름:</Text>
+            <Text style={patientInfoCardStyle.value}>{patientInfo?.name}</Text>
+          </View>
+        )}
+        {patientInfo?.gender && (
+          <View style={patientInfoCardStyle.labelValueWrapper}>
+            <Text style={patientInfoCardStyle.label}>성별:</Text>
+            <Text style={patientInfoCardStyle.value}>{gender}</Text>
+          </View>
+        )}
+        {patientInfo?.birthday && (
+          <View style={patientInfoCardStyle.labelValueWrapper}>
+            <Text style={patientInfoCardStyle.label}>생년월일:</Text>
+            <Text style={patientInfoCardStyle.value}>{patientInfo?.birthday}</Text>
+          </View>
+        )}
+        {patientInfo?.phoneNumber && (
+          <View style={patientInfoCardStyle.labelValueWrapper}>
+            <Text style={patientInfoCardStyle.label}>전화번호:</Text>
+            <Text style={patientInfoCardStyle.value}>{patientInfo?.phoneNumber}</Text>
+          </View>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default PatientInfoCard;
---
+++ src/pages/PatientDetailPage/styles/patientInfoCard.style.ts
@@ -0,0 +1,48 @@
+import { StyleSheet } from 'react-native';
+import { FILLBOM_COLOR } from '@/constants/color';
+import TEXT_STYLES from '@/styles/textStyles';
+
+const patientInfoCardStyle = StyleSheet.create({
+  Wrapper: {
+    width: 350,
+    height: 150,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 16,
+    justifyContent: 'flex-start',
+    alignSelf: 'center',
+    borderRadius: 10,
+    backgroundColor: FILLBOM_COLOR.GRAY[100],
+    paddingVertical: 16,
+    paddingLeft: 20,
+    marginVertical: 12,
+    marginHorizontal: 20,
+  },
+  profileImage: {
+    width: 92,
+    height: 92,
+    borderRadius: 50,
+    backgroundColor: FILLBOM_COLOR.GRAY[500],
+  },
+  patientInfoTextWrapper: {
+    marginLeft: 16,
+    justifyContent: 'center',
+    gap: 10,
+  },
+  label: {
+    ...TEXT_STYLES.BODY_MEDIUM_MEDIUM,
+    color: FILLBOM_COLOR.GRAY[600],
+  },
+  value: {
+    ...TEXT_STYLES.BODY_MEDIUM_SEMI_BOLD,
+    color: FILLBOM_COLOR.GRAY[900],
+    overflow: 'hidden',
+  },
+  labelValueWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+});
+
+export default patientInfoCardStyle;
