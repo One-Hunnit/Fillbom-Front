e03d88deb6cade26a20b2a1ccf46f2a457659fd3
hyorish03 <hyorish2001@gmail.com>
2025-02-18 18:39:48 +0900

    feat(OK-23): 환자 상세 데이터 타입 정의

--- src/pages/PatientDetailPage/hooks/useGetPatientDetail.ts
+++ src/pages/PatientDetailPage/hooks/useGetPatientDetail.ts
@@ -1,4 +1,7 @@
 import { useGetQuery } from '@/api/hooks';
+import { type components } from '@/api/types';
+
+export type PatientDetailData = components['schemas']['ResponseDtoDetail']['data'];
 
 const useGetPatientDetail = (patientId: number) => {
   const { data, isLoading, error, isError } = useGetQuery('/patients/{patientId}', {
@@ -9,7 +12,9 @@ const useGetPatientDetail = (patientId: number) => {
     },
   });
 
-  return { data, isLoading, error, isError };
+  const responseData = (data?.data?.data as PatientDetailData) || undefined;
+
+  return { data: responseData, isLoading, error, isError };
 };
 
 export default useGetPatientDetail;
