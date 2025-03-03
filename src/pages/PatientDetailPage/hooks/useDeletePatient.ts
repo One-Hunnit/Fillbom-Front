366a06f8e8d3733c87610afc3907846cf1783550
hyorish03 <hyorish2001@gmail.com>
2025-03-03 16:39:03 +0900

    feat(OK-23): 환자 삭제 기능을 위한 커스텀 훅 구현

--- src/pages/PatientDetailPage/hooks/useDeletePatient.ts
+++ src/pages/PatientDetailPage/hooks/useDeletePatient.ts
@@ -1,3 +1,22 @@
-/** @Todo
- * 환자 삭제 API 개발 시 구현
- */
+import { useDeleteMutation } from '@/api/hooks';
+
+const useDeletePatient = (patientId: number) => {
+  const mutation = useDeleteMutation('/caregiver/patients/{patientId}', {
+    onSuccess: () => {
+      console.log('환자를 삭제했습니다.');
+    },
+  });
+
+  const deletePatient = () => {
+    mutation.mutate({
+      params: {
+        path: {
+          patientId: patientId,
+        },
+      },
+    });
+  };
+
+  return { deletePatient };
+};
+export default useDeletePatient;
